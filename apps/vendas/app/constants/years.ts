ac09d360a4745361e0121588dfcbc368fdb94ada
Bruno Panizzi de Quadros <brunopanizziq@gmail.com>
2026-01-07 22:50:34 -0300

    add 2026

--- apps/vendas/app/constants/years.ts
+++ apps/vendas/app/constants/years.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
 
-export const years = [2024, 2025] as const
+export const years = [2024, 2025, 2026] as const
 
 export const yearSchema = (config?: z.RawCreateParams) =>
-  z.union([z.literal(2024), z.literal(2025)], config)
+  z.union([z.literal(2024), z.literal(2025), z.literal(2026)], config)
+
